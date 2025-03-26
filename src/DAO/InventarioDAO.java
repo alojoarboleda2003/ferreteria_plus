0da7d4c7ed3aacc272b043e515c7e00d74933c46
Juan Pablo Montaño <jotapemontano13@gmail.com>
2025-03-26 00:42:48 -0500

    form

--- src/DAO/InventarioDAO.java
+++ src/DAO/InventarioDAO.java
@@ -1,29 +1,10 @@
 package DAO;
 
+import conexionBD.ConexionBD;
+
 public class InventarioDAO
 {
-    public void insertarInventario()
-    {
-        System.out.println("Insertando inventario...");
-    }
+    private ConexionBD conexionBD = new ConexionBD();
 
-    public void eliminarInventario()
-    {
-        System.out.println("Eliminando inventario...");
-    }
 
-    public void actualizarInventario()
-    {
-        System.out.println("Actualizando inventario...");
-    }
-
-    public void buscarInventario()
-    {
-        System.out.println("Buscando inventario...");
-    }
-
-    public void listarInventarios()
-    {
-        System.out.println("Listando inventarios...");
-    }
 }
--- src/formulariosGUI/FormEmpleados.java
+++ src/formulariosGUI/FormEmpleados.java
@@ -1,9 +1,147 @@
 package formulariosGUI;
+import DAO.EmpleadoDAO;
+import conexionBD.ConexionBD;
+import modelos.Empleado;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class FormEmpleados
-{
-
+public class FormEmpleados {
     private JPanel Fempleados;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton ELIMINARButton;
+    private JButton GUARDARButton;
+    private JButton ACTUALIZARButton;
+    int filas = 0;
+
+    EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+
+    public FormEmpleados() {
+        obtener_datos();
+        GUARDARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nombre = textField2.getText();
+                double salario = Double.parseDouble(textField3.getText());
+                String cargo = textField4.getText();
+
+                Empleado empleado = new Empleado(0, nombre,cargo,salario);
+                empleadoDAO.agregar(empleado);
+                obtener_datos();
+                clear();
+
+            }
+        });
+        ACTUALIZARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nombre = textField2.getText();
+                double salario = Double.parseDouble(textField3.getText());
+                String cargo = textField4.getText();
+                int id = Integer.parseInt(textField1.getText());
+
+                Empleado empleado = new Empleado(id, nombre,salario,cargo);
+                empleadoDAO.actualizar(empleado);
+                obtener_datos();
+                clear();
+
+            }
+        });
+        ELIMINARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = Integer.parseInt(textField1.getText());
+                empleadoDAO.eliminar(id);
+                obtener_datos();
+                clear();
+            }
+
+
+        });
+
+        table1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                int selectFila = table1.getSelectedRow();
+
+                if (selectFila >= 0) {
+                    textField1.setText((String) table1.getValueAt(selectFila, 0));
+                    textField2.setText((String) table1.getValueAt(selectFila, 1));
+                    textField3.setText((String) table1.getValueAt(selectFila, 2));
+                    textField4.setText((String) table1.getValueAt(selectFila, 3));
+                    textField5.setText((String) table1.getValueAt(selectFila, 4));
+
+                    filas = selectFila;
+                }
+            }
+        });
+    }
+
+    public void clear() {
+        textField1.setText("");
+        textField2.setText("");
+        textField3.setText("");
+        textField4.setText("");
+        textField5.setText("");
+    }
+
+    ConexionBD conexionBD = new ConexionBD();
+
+
+    public void obtener_datos() {
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.addColumn("ID Empleado");
+        model.addColumn("Nombre");
+        model.addColumn("Cargo");
+        model.addColumn("Salario");
+
+        table1.setModel(model);
+        String[] dato = new String[5];
+        Connection con = conexionBD.getConnection();
+
+        try {
+            Statement start = con.createStatement();
+            String query = "SELECT * FROM empleado";
+            ResultSet rs = start.executeQuery(query);
+
+            while (rs.next()) {
+                dato[0] = rs.getString(1);
+                dato[1] = rs.getString(2);
+                dato[2] = rs.getString(3);
+                dato[3] = rs.getString(4);
+                dato[4] = rs.getString(5);
+                model.addRow(dato);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("empleado");
+        frame.setContentPane(new FormEmpleados().Fempleados);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(1006, 400);
+        frame.setResizable(false);
+    }
 }
+
